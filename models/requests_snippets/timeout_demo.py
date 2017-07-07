aff02833215459558cb210d9eccc1a7a5bec65e6
Wythe Zhou <binderclip2014@gmail.com>
2017-07-07 17:17:54 +0800

    add request time

--- models/requests_snippets/timeout_demo.py
+++ models/requests_snippets/timeout_demo.py
@@ -5,8 +5,9 @@ from requests.exceptions import Timeout
 
 def main():
     try:
-        r = requests.get('http://httpbin.org/delay/3', timeout=(3, 3))
+        r = requests.get('http://httpbin.org/delay/1', timeout=(3, 5))
         print(r.status_code)
+        print int(r.elapsed.total_seconds() * 1000)     # ms
     except Timeout:
         print("timeout")
 
