1b33b07c6f26483f006813801a4b1993beb0b8b5
Wythe Zhou <binderclip2014@gmail.com>
2017-07-18 17:35:07 +0800

    update set

--- basic/set_demo.py
+++ basic/set_demo.py
@@ -12,6 +12,7 @@ def init_set():
 
 
 def union_add_intersection():
+    print("===== union_add_intersection =====")
     s = set([1, 2, 3])
     s2 = set([2, 3, 4])
     print(s.intersection(s2))
@@ -20,9 +21,19 @@ def union_add_intersection():
     print(s)
 
 
+def add_remove():
+    print("===== set_add_remove =====")
+    s = set([1, 2, 3])
+    s.add(4)
+    s.remove(1)
+    print(s)
+
+
 def main():
     init_set()
     union_add_intersection()
+    add_remove()
+
 
 if __name__ == '__main__':
     main()
