071fdc748b7accbbd04d8ad112aa037708c1b2bb
Wythe Zhou <binderclip2014@gmail.com>
2017-07-26 16:01:25 +0800

    update redis hash

--- models/redis_snippets/redis_hash.py
+++ models/redis_snippets/redis_hash.py
@@ -2,12 +2,24 @@
 import redis
 
 hash_key = 'my_hash'
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+def set_data():
+    r.hset(hash_key, 'foo', 'bar')
+    r.hset(hash_key, 'n', 100)
+
+
+def get_data():
+    print('foo: {}'.format(r.hget(hash_key, 'foo')))
+    print('bar: {}'.format(r.hget(hash_key, 'bar')))
+    print('n: {}'.format(r.hget(hash_key, 'n')))
+    print('all: {}'.format(r.hgetall(hash_key)))
 
 
 def main():
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
-    r.hset(hash_key, 'hello', 'world')
-    print('Hello, {}!'.format(r.hget(hash_key, 'hello')))
+    set_data()
+    get_data()
     r.delete(hash_key)
 
 if __name__ == '__main__':
