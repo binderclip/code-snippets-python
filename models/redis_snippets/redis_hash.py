3ac73f8f6bc02a23f065ca44ce59f03ffee96e8f
Wythe Zhou <binderclip2014@gmail.com>
2017-07-26 16:06:06 +0800

    redis hrem

--- models/redis_snippets/redis_hash.py
+++ models/redis_snippets/redis_hash.py
@@ -8,17 +8,27 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 def set_data():
     r.hset(hash_key, 'foo', 'bar')
     r.hset(hash_key, 'n', 100)
+    r.hset(hash_key, 'a', 'aa')
+    r.hset(hash_key, 'b', 'bb')
 
 
 def get_data():
+    print('===== get data =====')
     print('foo: {}'.format(r.hget(hash_key, 'foo')))
     print('bar: {}'.format(r.hget(hash_key, 'bar')))
     print('n: {}'.format(r.hget(hash_key, 'n')))
     print('all: {}'.format(r.hgetall(hash_key)))
+    print('all keys: {}'.format(r.hgetall(hash_key).keys()))
+
+
+def rem_data():
+    r.hdel(hash_key, 'a')
 
 
 def main():
     set_data()
+    get_data()
+    rem_data()
     get_data()
     r.delete(hash_key)
 
