7691a03f6a55bd0b86ade5ae17b7c026ceb480a2
Wythe Zhou <binderclip2014@gmail.com>
2017-07-30 14:51:05 +0800

    update statsd

--- models/statsd_snippets/hello_statsd.py
+++ models/statsd_snippets/hello_statsd.py
@@ -6,6 +6,7 @@ def main():
     c = statsd.StatsClient('localhost', 8125)
     c.incr('foo')  # Increment the 'foo' counter.
     c.timing('stats.timed', 320)  # Record a 320ms 'stats.timed'.
+    c.timing('stats.timed,service_name={},status_code={}'.format('bar', 200), 120)
 
 
 if __name__ == '__main__':
