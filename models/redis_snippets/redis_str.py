43b29c936dbb6c3b100876aac29cc7089efac9ee
Wythe Zhou <binderclip2014@gmail.com>
2017-07-30 17:42:49 +0800

    update redis str

---
+++ models/redis_snippets/redis_str.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+import redis
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+def number():
+    print('===== number =====')
+    print('n: {}'.format(r.get('n')))
+    r.set('n', 1)
+    print('n: {}'.format(r.get('n')))
+    r.incr('n')
+    print('n: {}'.format(r.get('n')))
+    r.decr('n')
+    print('n: {}'.format(r.get('n')))
+    r.incr('n', 10)
+    print('n: {}'.format(r.get('n')))
+    r.decr('n', 2)
+    print('n: {}'.format(r.get('n')))
+    r.delete('n')
+
+
+def str_and_number():
+    print('===== str_and_number =====')
+    print('n: {}'.format(r.get('n')))
+    r.set('n', 1)
+    print('n: {}'.format(r.get('n')))
+    print('type n: {}'.format(type(r.get('n'))))
+    r.set('n', '1')
+    print('n: {}'.format(r.get('n')))
+    print('type n: {}'.format(type(r.get('n'))))
+    r.incr('n')
+    print('n: {}'.format(r.get('n')))
+    # r.set('n', 1.1)
+    # r.incr('n')     # value is not an integer or out of range
+    # r.set('n', 'x')
+    # r.incr('n')     # value is not an integer or out of range
+    r.delete('n')
+
+
+def main():
+    number()
+    str_and_number()
+
+
+if __name__ == '__main__':
+    main()
