cbd640ea04fe01b6ff35513bde5a2543bba23270
Wythe Zhou <binderclip2014@gmail.com>
2017-07-30 17:35:14 +0800

    update redis hash

--- models/redis_snippets/redis_hash.py
+++ models/redis_snippets/redis_hash.py
@@ -2,10 +2,12 @@
 import redis
 
 hash_key = 'my_hash'
+hash_key2 = 'my_hash2'
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
 def set_data():
+    print('===== set_data =====')
     r.hset(hash_key, 'foo', 'bar')
     r.hset(hash_key, 'n', 100)
     r.hset(hash_key, 'a', 'aa')
@@ -13,7 +15,7 @@ def set_data():
 
 
 def get_data():
-    print('===== get data =====')
+    print('===== get_data =====')
     print('foo: {}'.format(r.hget(hash_key, 'foo')))
     print('bar: {}'.format(r.hget(hash_key, 'bar')))
     print('n: {}'.format(r.hget(hash_key, 'n')))
@@ -22,7 +24,17 @@ def get_data():
 
 
 def rem_data():
+    print('===== rem_data =====')
     r.hdel(hash_key, 'a')
+
+
+def set_hash_to_str():
+    print('===== set_hash_to_str =====')
+    r.set(hash_key2, 'bar')
+    print('hash_key2: {}'.format(r.get(hash_key2)))
+    r.delete(hash_key2)     # 不删除的话就会 WRONGTYPE Operation
+    r.hset(hash_key2, 'foo', 'bar')
+    print('hash_key2: {}'.format(r.hgetall(hash_key2)))
 
 
 def main():
@@ -30,6 +42,7 @@ def main():
     get_data()
     rem_data()
     get_data()
+    set_hash_to_str()
     r.delete(hash_key)
 
 if __name__ == '__main__':
