570e125e2e501ae777c9e839ab27dc42cbef3ff1
Wythe Zhou <binderclip2014@gmail.com>
2017-08-27 19:15:44 +0800

    update write and read

--- models/filesystem/read_write_file.py
+++ models/filesystem/read_write_file.py
@@ -1,12 +1,28 @@
 # coding: utf-8
 
 
-def main():
+def write_and_read():
+    print("=== write_and_read ===")
     with open('temp.txt', 'w') as f:
-        f.writelines('Hello\nWorld')
+        f.write('hello\nworld')
     with open('temp.txt', 'r') as f:
         lines = f.readlines()
         print('Lines:\n{}'.format(lines))
+
+
+def write_with_line_break_and_read():
+    print("=== write_with_line_break_and_read ===")
+    lines = ['hello', 'world']
+    with open('temp.txt', 'w') as f:
+        f.writelines(line + '\n' for line in lines)
+    with open('temp.txt', 'r') as f:
+        lines = f.readlines()
+        print('Lines:\n{}'.format(lines))
+
+
+def main():
+    write_and_read()
+    write_with_line_break_and_read()
 
 
 if __name__ == '__main__':
