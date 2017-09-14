fda72dcdda5ad77c5994fcea0b4a324dac0e46b6
Wythe Zhou <binderclip2014@gmail.com>
2017-09-14 19:05:10 +0800

    update argparse

--- models/argparse_snippets/argparse_demo.py
+++ models/argparse_snippets/argparse_demo.py
@@ -5,11 +5,13 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--debug", action="store_true", help='debug mode on')
+    parser.add_argument("your_name", help="your name")
 
     args = parser.parse_args()
 
     print args
     print args.debug
+    print args.your_name
 
 
 if __name__ == '__main__':
