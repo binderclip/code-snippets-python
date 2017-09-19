2e3935500a7757397a1cb0e981bb874723046304
Wythe Zhou <binderclip2014@gmail.com>
2017-09-19 12:44:03 +0800

    add decorator

---
+++ models/decorator_snippets/retry_with_decorator.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+import requests
+
+
+def retries(times=3):
+    def wrap(func):
+        def r(*args, **kw):
+            t = 0
+            while True:
+                try:
+                    return func(*args, **kw)
+                except requests.exceptions.ConnectTimeout as e:
+                    t += 1
+                    if t < times:
+                        print('retry {}'.format(t))
+                    else:
+                        raise e
+        return r
+    return wrap
+
+
+@retries()
+def make_a_get():
+    return requests.get('http://httpbin.org/get', timeout=(0.3, 2))
+
+
+def main():
+    print make_a_get()
+
+
+if __name__ == '__main__':
+    main()
