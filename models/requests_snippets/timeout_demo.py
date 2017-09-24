c2d570171dd22dcd128b8be6156abaf63e74a398
Wythe Zhou <binderclip2014@gmail.com>
2017-09-24 15:18:34 +0800

    update requests timeout demo

--- models/requests_snippets/timeout_demo.py
+++ models/requests_snippets/timeout_demo.py
@@ -5,10 +5,14 @@ from requests.exceptions import Timeout
 
 def main():
     try:
-        r = requests.get('http://httpbin.org/delay/1', timeout=(3, 5))
+        r = requests.get('http://httpbin.org/delay/1', timeout=(3, 5))  # ConnectTimeout = 3, ReadTimeout = 5
+        # r = requests.get('http://httpbin.org/delay/1', timeout=5)       # ConnectTimeout = ReadTimeout = 5
         print(r.status_code)
         print int(r.elapsed.total_seconds() * 1000)     # ms
     except Timeout:
+        # http://docs.python-requests.org/en/master/_modules/requests/exceptions/
+        # ConnectTimeout(ConnectionError, Timeout)
+        # ReadTimeout(Timeout)
         print("timeout")
 
 
