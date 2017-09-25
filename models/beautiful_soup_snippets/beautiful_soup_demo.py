ef3ef6106872c561f3eae9289552fc662d2d4e71
Wythe Zhou <binderclip2014@gmail.com>
2017-09-25 16:25:41 +0800

    update beautiful_soup

--- models/beautiful_soup_snippets/beautiful_soup_demo.py
+++ models/beautiful_soup_snippets/beautiful_soup_demo.py
@@ -1,8 +1,11 @@
 # coding: utf-8
+import simplejson
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString, Tag
 
 
-def main():
+def parse_whole_html():
+    print("=== parse_whole_html ===")
     html_doc = """
 <html><head><title>The Dormouse's story</title></head>
 <body>
@@ -22,6 +25,67 @@ and they lived at the bottom of a well.</p>
     print("soup.title.name: {}".format(soup.title.name))
     print("soup.title.string: {}".format(soup.title.string))
     print("soup.title.parent.name: {}".format(soup.title.parent.name))
+
+
+def parse_part_html():
+    print("=== parse_part_html ===")
+    html_para = """<p>just <b>click</b> <a href="http://example.com">here</a>.</p>"""
+    soup = BeautifulSoup(html_para, 'html.parser')
+    for s in soup.strings:
+        print(s)
+    print("".join(soup.strings))
+    for content in soup.contents:
+        print_content(content)
+
+
+def print_content(content, level=0):
+    print("{}{}".format("\t" * level, content))
+    if isinstance(content, NavigableString):
+        return
+    for sub_content in content.contents:
+        print_content(sub_content, level + 1)
+    print('{}<<< end'.format("\t" * level))
+
+
+def parse_part_html2():
+    print("=== parse_part_html2 ===")
+    html_para = """<p>just <b>click</b> <a href="http://example.com">here</a>.</p>"""
+    soup = BeautifulSoup(html_para, 'html.parser')
+    print(simplejson.dumps(parse_soup_to_markup(soup)))
+
+
+def parse_soup_to_markup(soup):
+    m = {
+        "text": "".join(soup.strings),
+    }
+    markups, _ = get_markups(soup)
+    m["markups"] = markups
+    return m
+
+
+def get_markups(content, start_index=0):
+    if isinstance(content, NavigableString):
+        start_index += len(content)
+        return [], start_index
+    # TODO: 确定剩下的是不是都是 tag 或 soup，且是不是可行
+    _start_index = start_index
+    markups = []
+    for child in content.children:
+        new_markups, start_index = get_markups(child, start_index)
+        markups.extend(new_markups)
+    if isinstance(content, Tag) and content.name in ('a', 'b'):
+        markup = {
+            "name": content.name,
+            "start_index": _start_index
+        }
+        markup['end_index'] = start_index
+        markups.append(markup)
+    return markups, start_index
+
+def main():
+    parse_whole_html()
+    parse_part_html()
+    parse_part_html2()
 
 
 if __name__ == '__main__':
