d545cc6f21ea04dcf32166d096466c8500bfc0fc
Wythe Zhou <binderclip2014@gmail.com>
2017-09-28 12:13:16 +0800

    update simplejson loads

--- models/simplejson_snippets/hello_simplejson.py
+++ models/simplejson_snippets/hello_simplejson.py
@@ -2,12 +2,25 @@
 import simplejson
 
 
-def main():
+def dump_loads():
+    print("=== dump_loads ===")
     d = {"hello": "world"}
     s = simplejson.dumps(d)
     d2 = simplejson.loads(s)
     print(s)
     print(d2)
+
+
+def dump_str_escape_test():
+    print("=== dump_str_escape_test ===")
+    d = {"hello": "world > &"}
+    s = simplejson.dumps(d)
+    print(s)
+
+
+def main():
+    dump_loads()
+    dump_str_escape_test()
 
 
 if __name__ == '__main__':
