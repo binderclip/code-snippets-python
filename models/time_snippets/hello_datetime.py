c196dc544c0ad5e8e20f79ec1e49664f2506bda8
Wythe Zhou <binderclip2014@gmail.com>
2017-10-11 19:56:37 +0800

    update datetime

--- models/time_snippets/hello_datetime.py
+++ models/time_snippets/hello_datetime.py
@@ -4,8 +4,10 @@ import datetime
 
 def main():
     time_delta = datetime.datetime(2017, 7, 17) - datetime.datetime(2017, 7, 20)
+    time_delta2 = datetime.timedelta(days=1)
     print(time_delta)
     print(abs(time_delta))
+    print(time_delta2)
 
 
 if __name__ == '__main__':
