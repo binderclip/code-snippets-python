9f3f306c71b8b0570292dc459685c575d463a03f
Wythe Zhou <binderclip2014@gmail.com>
2017-10-24 20:56:17 +0800

    add tuple

---
+++ models/tuple_snippets/hello_tuple.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+
+def fun1():
+    return 1, 2
+
+
+def fun2():
+    return (3, 4)
+
+
+def main():
+    print(fun1())
+    print(fun2())
+
+
+if __name__ == '__main__':
+    main()
