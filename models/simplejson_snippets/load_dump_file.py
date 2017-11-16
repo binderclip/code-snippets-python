4bb1956444d87cf820b7a4b8de0659ee980d35cb
clip <binderclip2014@gmail.com>
2017-11-16 21:23:58 +0800

    add dump file

--- models/simplejson_snippets/load_dump_file.py
+++ models/simplejson_snippets/load_dump_file.py
@@ -6,6 +6,8 @@ def main():
     with open("hello.json") as f:
         s = simplejson.load(f)
         print(s)
+    with open("hello.json", 'w') as f:
+        simplejson.dump({}, f)
 
 
 if __name__ == '__main__':
