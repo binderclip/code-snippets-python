db20348bd9fd5a242be11865d6a42659effd5829
clip <binderclip2014@gmail.com>
2017-11-16 15:45:36 +0800

    update exception

--- models/exception_snippets/hello_exception.py
+++ models/exception_snippets/hello_exception.py
@@ -2,27 +2,28 @@
 
 
 def maybe_error(n, raise_it=False):
-    print('maybe_error == start ==')
+    print('maybe_error == start == {}'.format(n))
     a = None
     try:
         a = 10 / n
     except Exception as e:
-        print('maybe_error == except ==')
+        print('maybe_error == except == {}'.format(n))
         print('e: {}'.format(e))
         if raise_it:
-            print('maybe_error == raise ==')
+            print('maybe_error == raise == {}'.format(n))
             raise e
     else:
-        print('maybe_error == else ==')
-    print('a = {}'.format(a))
-    print('maybe_error == end ==')
+        print('maybe_error == else == {}'.format(n))
+    finally:
+        print('maybe_error == finally == {}'.format(n))
+    print('maybe_error == end == {}'.format(n))
 
 
 def box(n, raise_it):
     try:
         maybe_error(n, raise_it)
     except Exception:
-        pass
+        print('box == except == {}'.format(n))
 
 
 def main():
