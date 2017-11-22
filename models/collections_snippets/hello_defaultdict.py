1e0b1b1c7152ba8c0ae42c0ceae956ccbaea9b02
clip <binderclip2014@gmail.com>
2017-11-22 20:28:20 +0800

    add defaultdict

---
+++ models/collections_snippets/hello_defaultdict.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from collections import defaultdict
+
+
+def main():
+    print(int(), int())
+    dd = defaultdict(int)
+    print(dd['a'], dd['b'])
+
+    print(str(), str())
+    dd = defaultdict(str, {'a': 'aa'})
+    print(dd['a'], dd['b'])
+
+    print(bool(), bool())
+    dd = defaultdict(bool)
+    print(dd['a'], dd['b'])
+
+    dd = defaultdict(lambda: 3)
+    print(dd['a'], dd['b'])
+
+
+if __name__ == '__main__':
+    main()
