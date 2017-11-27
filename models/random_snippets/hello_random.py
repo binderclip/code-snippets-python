77a673ec36f9a2e8d3716e233d3a526458943676
clip <binderclip2014@gmail.com>
2017-11-27 11:15:07 +0800

    update random

--- models/random_snippets/hello_random.py
+++ models/random_snippets/hello_random.py
@@ -2,11 +2,31 @@
 import random
 
 
-def main():
+def test_randrange():
+    print('=== test_randrange ===')
     s = ''
     for i in xrange(100):
         s += '{} '.format(random.randrange(5))
     print(s)
+
+    s = ''
+    for i in xrange(100):
+        s += '{} '.format(random.randrange(start=1, stop=3, step=1))
+    print(s)
+
+
+def test_sample():
+    print('=== test_sample ===')
+    l = range(1, 5)
+    s = ''
+    for i in xrange(100):
+        s += '{} '.format(random.sample(l, 2))
+    print s
+
+
+def main():
+    test_randrange()
+    test_sample()
 
 
 if __name__ == '__main__':
