31579308ee940c10acc53e350551574329c7de3a
clip <binderclip2014@gmail.com>
2017-11-30 23:50:15 +0800

    add counter

---
+++ models/collections_snippets/hello_counter.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from collections import Counter
+
+
+def main():
+    cnt = Counter()
+    for n in [1, 2, 3, 2, 1, 4, 7, 6, 2]:
+        cnt[n] += 1
+    print cnt.items()
+
+
+if __name__ == '__main__':
+    main()
