6a3f459192c35fee8d69a2efb979b450302b1f00
clip <binderclip2014@gmail.com>
2017-12-04 10:45:26 +0800

    add run file

---
+++ basic/run_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def main():
+    print('Hello')
+
+
+if __name__ == '__main__':
+    main()
+
+#!/path/to/interpreter
+# chmod +x my_python_script.py
+# https://www.pythoncentral.io/execute-python-script-file-shell/
