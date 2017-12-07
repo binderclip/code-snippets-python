88b0eef0ea1772a4a94ccf66ade3aa64ca9bdd7e
clip <binderclip2014@gmail.com>
2017-12-07 19:25:16 +0800

    update config

--- models/config_snippets/config/__init__.py
+++ models/config_snippets/config/__init__.py
@@ -3,14 +3,14 @@ import os
 
 __all__ = ['CSConfig']
 
-app_config_module_path = os.environ.get('CS_CONFIG', '')
-if not app_config_module_path:
-    raise Exception('No CS_CONFIG in os environ, should be something like: config_snippets.config.xxx_config')
-app_config_module = __import__(app_config_module_path, fromlist=['Config'])
+app_config_file_name = os.environ.get('CS_CONFIG', '')
+if not app_config_file_name:
+    raise Exception('No CS_CONFIG in os environ, should be something like: xxx_config')
+app_config_module = __import__('config.{}'.format(app_config_file_name), fromlist=['Config'])
 
 CSConfig = app_config_module.Config
 
-# should export in shell: export CS_CONFIG=config.xxx_config
+# should export in shell: export CS_CONFIG=xxx_config
 # in gitignore:
 # #/models/config_snippets/config/*_config.py
 # !/models/config_snippets/config/sample_config.py
