ac421738736e80f827afb13b2b47431a4d7a75eb
clip <binderclip2014@gmail.com>
2017-12-08 14:40:00 +0800

    add re

--- models/re_snippets/re_collect.py
+++ models/re_snippets/re_collect.py
@@ -10,7 +10,14 @@ def find_and_print(order, r, s, group=0):
 def main():
     s = '- [1.1.1 连接管理与安全性](#1.1.1 连接管理与安全性)'   # to find 1.1.1 连接管理与安全性
     find_and_print('A', r"\(#(.*)\)", s, 1)
+    r_url = r"^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*‌​)*(\/?)([a-zA-Z0-9\-‌​\.\?\,\'\/\\\+&amp;%‌​\$#_]*)?$"
+    s = 'xxx http://exp.com'    # to find None
+    find_and_print('B', r_url, s, 0)
+    s = 'http://exp.com'        # to find http://exp.com
+    find_and_print('B', r_url, s, 0)
 
 
 if __name__ == '__main__':
     main()
+
+# https://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url
