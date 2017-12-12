5473e7a1ec0f18992370f6650644841cb93b09ab
clip <binderclip2014@gmail.com>
2017-12-12 11:07:06 +0800

    update show progress

--- utils/002-show-progress/app.py
+++ utils/002-show-progress/app.py
@@ -8,7 +8,7 @@ def main():
     n = 15
     l = range(n)
     for i, _ in enumerate(l):
-        sys.stdout.write('\r{}'.format(i))
+        sys.stdout.write('\r{}/{} ({:.2f}%)'.format(i + 1, n, 100.0 * (i + 1) / n))
         time.sleep(0.1)
     print('\n=== end ===')
 
