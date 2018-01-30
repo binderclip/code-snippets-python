effac334af9d412291f7e9709b7692b6aac8a4f1
clip <binderclip2014@gmail.com>
2018-01-30 18:25:33 +0800

    update range

---
+++ basic/range_py3.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+
+def main():
+    print(range(0))
+    print(range(1))
+    print(range(0, 1))
+    print(range(0, 1, 1))
+    print(range(10, 15, 4))
+    print(range(-2))
+    print(range(0, -2, -1))
+
+    print(list(range(10)))
+
+
+if __name__ == '__main__':
+    main()
+
+# https://docs.python.org/3/library/functions.html#func-range
