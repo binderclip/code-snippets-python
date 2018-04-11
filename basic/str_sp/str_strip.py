4ccb686237521d3156883b2a71ace98261b5177b
clip <binderclip2014@gmail.com>
2018-04-11 20:07:16 +0800

    update str

--- basic/str_sp/str_strip.py
+++ basic/str_sp/str_strip.py
@@ -1,10 +1,7 @@
-# coding: utf-8
-
-
 def main():
     print(repr('  Hello\n\r'))
     print(repr('  Hello\n\r'.strip()))
-    print('=== end ===')
+    print('x大西瓜'.strip('大瓜西'))
 
 
 if __name__ == '__main__':
