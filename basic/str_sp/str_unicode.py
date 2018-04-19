5c0f53156717c542090b933f23440c8af9a0fd31
clip <binderclip2014@gmail.com>
2018-04-19 12:08:31 +0800

    update string

--- basic/str_sp/str_unicode.py
+++ basic/str_sp/str_unicode.py
@@ -1,20 +1,19 @@
-# coding: utf-8
-
 def to_utf8(s):
-    if isinstance(s, unicode):
+    if isinstance(s, str):
         return s.encode('utf-8')
     return s
 
 
 def to_unicode(s):
-    if isinstance(s, str):
+    if isinstance(s, bytes):
         return s.decode('utf-8')
     return s
 
 
 def main():
-    print(to_utf8('哈哈哈'), to_unicode('哈哈哈'))
-    print(to_utf8(u'哈哈哈'), to_unicode(u'哈哈哈'))
+    print(to_utf8('哈哈哈'), to_unicode(b'\xe5\x93\x88\xe5\x93\x88\xe5\x93\x88'))
+
+    # print('哈哈哈'.encode('ascii'))  # UnicodeEncodeError: 'ascii' codec can't encode characters in position 0-2: ordinal not in range(128)
 
 
 if __name__ == '__main__':
---
+++ basic/str_sp/upper_sp.py
@@ -0,0 +1,10 @@
+def main():
+    s = 'hello world'
+    print(s.upper())
+    print(s.capitalize())
+    print(s.capitalize().swapcase())
+    print(s.title())
+
+
+if __name__ == '__main__':
+    main()
