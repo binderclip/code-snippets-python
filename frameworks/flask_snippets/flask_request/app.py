edca8d1266e2ec7dfe1db0b054592198dd9d5209
clip <binderclip2014@gmail.com>
2018-05-04 15:32:35 +0800

    add flask request

---
+++ frameworks/flask_snippets/flask_request/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def get():
+    return "Hello World!"
+
+
+
+@app.route("/get", methods=['GET'])
+def app_get():
+    return jsonify({
+        "args": request.args,
+    })
+
+
+@app.route("/post", methods=['POST'])
+def app_post():
+    return jsonify({
+        "json": request.json,
+        "form": request.form,
+    })
+
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
