8f690a1130638374421f406fd2a24efd3b2a03a6
clip <binderclip2014@gmail.com>
2018-05-04 16:07:34 +0800

    add flask error handler

--- frameworks/flask_snippets/errorhandler_app/app.py
+++ frameworks/flask_snippets/errorhandler_app/app.py
@@ -2,6 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def app_404(e):
+    print(e)
+    print(type(e))
+    return 'page not found', 404
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
