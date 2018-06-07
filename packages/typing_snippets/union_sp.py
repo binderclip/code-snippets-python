59e61e1b2daa495095fa8acfa660e0aa96abf43f
clip <binderclip2014@gmail.com>
2018-06-07 11:00:07 +0800

    update

--- packages/typing_snippets/union_sp.py
+++ packages/typing_snippets/union_sp.py
@@ -1,15 +1,30 @@
 # coding: utf-8
-from typing import Union
+from typing import Union, Optional
 
 
 def foo(x: Union[str, int]) -> None:
     print(x)
 
 
+def foo2(x: int) -> Union[str, None]:
+    if x < 0:
+        return
+    return str(x)
+
+
+def foo3(x: int) -> Optional[str]:
+    return foo2(x)
+
+
 def main():
     foo(1)
     foo('a')
     # foo([1, 2])
+
+    foo2(10)
+    foo2(-10)
+    foo3(10)
+    foo3(-10)
 
 
 if __name__ == '__main__':
