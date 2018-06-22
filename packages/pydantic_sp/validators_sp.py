ee04b4317805ff4ec5ec70d72a3c8d333aafacb0
clip <binderclip2014@gmail.com>
2018-06-22 15:29:06 +0800

    pydantic

--- packages/pydantic_sp/validators_sp.py
+++ packages/pydantic_sp/validators_sp.py
@@ -31,7 +31,32 @@ class IntNextCursorDTO(BaseModel):
         return int(n)
 
 
-def main():
+class Foo(BaseModel):
+    n: int
+
+
+class Bar(BaseModel):
+    foo: Foo
+    n: int
+    m: int
+
+    @validator('m')
+    def m_less_than_n(cls, v, values, **kwargs):
+        print(f'm {values}')
+        if 'n' in values and v >= values['n']:
+            raise ValueError('m greater than n')
+        return v
+
+    @validator('n')
+    def n_same_with_n(cls, v, values, **kwargs):
+        print(f'n {values}')
+        if 'foo' in values and v != values["foo"].n:
+            raise ValueError('foo.n != n')
+        return v
+
+
+def basic_validator():
+    print('=== basic_validator ===')
     print(UserModel(name='samuel colvin', password1='zxcvbn', password2='zxcvbn'))
     # > UserModel name='Samuel Colvin' password1='zxcvbn' password2='zxcvbn'
 
@@ -53,8 +78,34 @@ def main():
     # name:
     #   must contain a space (error_type=ValueError track=ConstrainedStrValue)
 
+
+def validator_with_pre():
+    print('=== validator_with_pre ===')
     next_cursor = IntNextCursorDTO(cursor='Mg==', size='10')
     print(next_cursor)
+
+
+def validator_with_multi_fields():
+    print('=== validator_with_multi_fields ===')
+    print(Bar(n=10, m=5, foo=Foo(n=10)))
+    try:
+        print(Bar(n=10, m=15, foo=Foo(n=10)))
+    except ValidationError as e:
+        print(e)
+    try:
+        print(Bar(n=10, m=5, foo=Foo(n=11)))
+    except ValidationError as e:
+        print(e)
+    try:
+        print(Bar.parse_obj({"n": 10, "m": 5, "foo": {"n": 11}}))
+    except ValidationError as e:
+        print(e)
+
+
+def main():
+    basic_validator()
+    validator_with_pre()
+    validator_with_multi_fields()
 
 
 if __name__ == '__main__':
