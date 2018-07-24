54299a983824ac259953a3c7732c4fc87d46c5af
clip <binderclip2014@gmail.com>
2018-07-24 21:02:06 +0800

    pydantic

--- packages/pydantic_sp/setting_sp.py
+++ packages/pydantic_sp/setting_sp.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     redis_host = 'localhost'    # set os env APP_REDIS_HOST to overwrite
     redis_port = 6379
     redis_database = 0
+    debug = False   # 'true' and '1' are True, 'false' and '0' are False
 
     class Config:
         env_prefix = 'APP_'
@@ -14,6 +15,7 @@ def main():
     settings = Settings()
     print(settings.redis_host)
     print(settings.redis_port, type(settings.redis_port))
+    print(settings.debug)
 
 
 if __name__ == '__main__':
