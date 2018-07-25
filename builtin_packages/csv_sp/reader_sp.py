dd2690bd2b32b329075546ae91c125b87bd88493
clip <binderclip2014@gmail.com>
2018-07-25 16:48:37 +0800

    csv

--- builtin_packages/csv_sp/reader_sp.py
+++ builtin_packages/csv_sp/reader_sp.py
@@ -4,6 +4,7 @@ import csv
 def main():
     with open('some.csv', 'r', newline='') as f:
         reader = csv.reader(f)
+        next(reader)    # pass the first line
         for row in reader:
             print(row)
 
@@ -11,3 +12,4 @@ def main():
 if __name__ == '__main__':
     main()
 
+# https://stackoverflow.com/questions/11349333/when-processing-csv-data-how-do-i-ignore-the-first-line-of-data
