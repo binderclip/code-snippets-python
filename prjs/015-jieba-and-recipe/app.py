a9f1266ffba781454182e356ba7c3cc9565bd585
clip <binderclip2014@gmail.com>
2018-11-26 18:45:46 +0800

    jieba

--- prjs/015-jieba-and-recipe/app.py
+++ prjs/015-jieba-and-recipe/app.py
@@ -7,20 +7,34 @@ recipe_title_data = defaultdict(dict)
 
 
 def cut_txt(txt: str, mark: str):
-    seg_list = jieba.cut(txt, cut_all=False)
+    seg_list = jieba.cut_for_search(txt)
     # print(f"{txt} -> " + " | ".join(seg_list))  # 精确模式
     recipe_title_data[txt][mark] = "_".join(seg_list)
 
 
-def main():
-    titles = []
-    with open('recipe_titles.txt', 'r') as f:
+def get_titles_from_file():
+    with open('recipe_tstring_typesitles.txt', 'r') as f:
         lines = f.readlines()
         for line in lines:
             _line = line.strip()
             if not _line:
                 continue
-            titles.append(_line)
+            yield _line
+
+
+def get_titles():
+    return [
+        '鱼豆腐',
+        '方便面',
+        '意面',
+        '意大利面',
+    ]
+
+
+def main():
+    titles = []
+    # titles.extend(get_titles_from_file())
+    titles.extend(get_titles())
 
     for title in titles:
         cut_txt(title, 'c1')
@@ -35,11 +49,12 @@ def main():
     jieba.del_word('烧鸡')
     jieba.del_word('炖鸡')
     jieba.del_word('咖喱鸡')
+    jieba.del_word('大利')
 
     for title in titles:
         cut_txt(title, 'c2')
 
-    for txt, data in list(recipe_title_data.items())[1175:1200]:
+    for txt, data in list(recipe_title_data.items()):
         t1 = data["c1"]
         t2 = data["c2"]
         r = '==' if t1 == t2 else 'xx'
