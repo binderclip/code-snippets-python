c6c55bdce8f7bafebd13998b03a57c7fe0f0f284
clip <binderclip2014@gmail.com>
2019-01-11 15:06:50 +0800

    datetime timestamp

--- builtin_packages/datetime_sp/datetime_to_timestamp.py
+++ builtin_packages/datetime_sp/datetime_to_timestamp.py
@@ -4,8 +4,13 @@ import datetime
 
 def main():
     dt = datetime.datetime.now()
-    print(dt.timestamp())
+    print(dt.timestamp())   # python 2 中没有这个方法
     print(time.time())
+
+    d = datetime.datetime.now().date()
+    print(d)
+    print(datetime.datetime.combine(d, datetime.time()).timestamp())
+    print(time.mktime(d.timetuple()))
 
 
 if __name__ == '__main__':
