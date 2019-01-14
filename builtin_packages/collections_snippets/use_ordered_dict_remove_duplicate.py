982bdefb263edd4c2ad628570690149cbb61b367
clip <binderclip2014@gmail.com>
2019-01-14 21:17:14 +0800

    ordereddict

--- builtin_packages/collections_snippets/use_ordered_dict_remove_duplicate.py
+++ builtin_packages/collections_snippets/use_ordered_dict_remove_duplicate.py
@@ -2,14 +2,23 @@
 from collections import OrderedDict
 
 
-def remove_duplicate(items):
-    print("=== remove_duplicate ===")
-    return list(OrderedDict.fromkeys(items))
+def remove_duplicate1():
+    print("=== remove_duplicate1 ===")
+    ids = [1, 3, 4, 1, 2, 3]
+    print(list(OrderedDict.fromkeys(ids)))
+
+
+def remove_duplicate2():
+    print("=== remove_duplicate2 ===")
+
+    od = OrderedDict([(1, 'a'), (2, 'b'), (1, 'a')])
+    print([v for _, v in od.items()])
+    # return list(OrderedDict.fromkeys(items))
 
 
 def main():
-    ids = [1, 3, 4, 1, 2, 3]
-    print(remove_duplicate(ids))
+    remove_duplicate1()
+    remove_duplicate2()
 
 
 if __name__ == '__main__':
