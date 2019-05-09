d5359b43249836e45c45dba57c80c7034278c141
clip <binderclip2014@gmail.com>
2019-05-09 15:29:49 +0800

    update

--- packages/pypinyin_sp/pypinyin_sp.py
+++ packages/pypinyin_sp/pypinyin_sp.py
@@ -4,6 +4,8 @@ from pypinyin import lazy_pinyin
 def main():
     print(lazy_pinyin('中国'))
     print(''.join(lazy_pinyin('大西瓜')))
+    print(lazy_pinyin('T恤'))
+    print(lazy_pinyin('big西瓜373a'))
 
 
 if __name__ == '__main__':
