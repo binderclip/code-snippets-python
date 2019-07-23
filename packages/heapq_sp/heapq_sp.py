be0c3016aacbb6cf014011378dace79b83af9529
zhoubokun <zhoubokun@bytedance.com>
2019-07-23 14:12:45 +0800

    u
    Change-Id: I31a1d2edb2f22cbfcc169870c44389e4555b2cf7

--- packages/heapq_sp/heapq_sp.py
+++ packages/heapq_sp/heapq_sp.py
@@ -2,7 +2,8 @@ from heapq import *
 
 
 def main():
-    h = []
+    h = []  # 毕竟 heap 是可以用 list 实现的，所以这个 heapq 直接用 list 操作也没毛病
+    # 不确定有没有初始化的过程，以及中间强行破坏后会怎么样
     heappush(h, (5, 'write code'))
     heappush(h, (7, 'release product'))
     heappush(h, (1, 'write spec'))
